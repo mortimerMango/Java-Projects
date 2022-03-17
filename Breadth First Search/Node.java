85ae824e1d1b4e8289051ba1efd399f0bb29f5f6
mortimerMango <94403406+mortimerMango@users.noreply.github.com>
2022-03-17 13:38:34 -0700

    Update Node.java

--- Breadth First Search/Node.java
+++ Breadth First Search/Node.java
@@ -1,22 +1,22 @@
 
 public class Node<T>{
 	
-	public T data;				//hold whatever object to be used as a node
+	public T data;					//hold whatever object to be used as a node
 	public Node prev;
 	public Node next;
 	
-	public Node(T data, Node next) {
+	public Node(T data, Node next) {		//Parameterized Constructor 
 		this.data = data;
 		this.next = next;
 	}
 	
-	public Node(T data, Node _head, Node _tail) {
+	public Node(T data, Node _head, Node _tail) {	//Constructor used for Linked List
 		this.data = data;
 		prev = _head;
 		next = _tail;
 	}
 	
-	public String toString() {		//Value to be returned when toString() is called
+	public String toString() {			//Value to be returned when toString() is called
 		return "["+data+"]";
 	}
 }
